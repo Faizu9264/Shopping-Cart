38273b266d24138f6924b77539502bd5751d5a8b
FaizuRahman <www.faizu9264@gmail.com>
2022-12-28 09:41:30 +0530

    Password key

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,28 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 const productHelpers = require('../helpers/product-helpers');
+const userHelpers=require('../helpers/user-helpers')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   productHelpers.getAllProduct().then((products)=>{
     console.log(products);
-    res.render('user/view-products',{products,admin:true})
+    res.render('user/view-products',{products})
   })
 });
+router.get('/login',(req,res)=>{
+  res.render('user/login')
+
+})
+router.get('/signup',(req,res)=>{
+  res.render('user/signup')
+
+})
+router.post('/signupaction',(req,res)=>{
+  console.log("test");
+  userHelpers.doSignup(req.body).then((response)=>{
+    console.log(response);
+  })
+})
 
 module.exports = router;
