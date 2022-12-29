076090a489f79925d7569a128e8e02ca2e811868
FaizuRahman <www.faizu9264@gmail.com>
2022-12-29 20:59:54 +0530

    Edit image,Add to cart,Aggregation

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,6 +1,7 @@
 var db=require('../config/connection')
 var collection=require('../config/collections');
 const { ObjectID } = require('bson');
+const { response } = require('../app');
 var objectId=require('mongodb').ObjectID
 module.exports={
     addProduct:(product,callback)=>{
@@ -22,6 +23,27 @@ module.exports={
             db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({_id:objectId(prodId)}).then((response)=>{
                 // console.log(response);
                 resolve(response)
+            })
+        })
+    },
+    getProductDetails:(proId)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:objectId(proId)}).then((product)=>{
+                resolve(product)
+            })
+        })
+    },
+    updateProduct:(proId,proDetails)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).updateOne({_id:objectId(proId)},{
+                $set:{
+                    Name:proDetails.Name,
+                    Description:proDetails.Description,
+                    Price:proDetails.Price,
+                    Category:proDetails.Category
+                }
+            }).then((response)=>{
+                resolve()
             })
         })
     }
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 const productHelpers = require('../helpers/product-helpers');
 const userHelpers=require('../helpers/user-helpers')
-const verifyLogin=(req,res)=>{
+const verifyLogin=(req,res,next)=>{
   if (req.session.loggedIn) {
     next()
   }else{
@@ -32,10 +32,14 @@ router.get('/signup',(req,res)=>{
   res.render('user/signup')
 
 })
-router.post('/signupaction',(req,res)=>{
+router.post('/signup',(req,res)=>{
   console.log("test");
   userHelpers.doSignup(req.body).then((response)=>{
     console.log(response);
+    req.session.loggedIn=true
+    req.session.user=response
+    res.redirect('/')
+    
   })
 })
 router.post('/login',(req,res)=>{
@@ -54,8 +58,15 @@ router.get('/logout',(req,res)=>{
   req.session.destroy()
   res.redirect('/')
 })
-router.get('/cart',verifyLogin,(req,res)=>{
+router.get('/cart',verifyLogin,async(req,res)=>{
+  let product=await userHelpers.getCartProducts(req.session.user._id)
+  console.log(product);
   res.render('user/cart')
+})
+router.get('/add-to-cart/:id',verifyLogin,(req,res)=>{
+  userHelpers.addToCart(req.params.id,req.session.user._id).then(()=>{
+    res.redirect('/')
+  })
 })
 
 
